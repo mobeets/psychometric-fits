8b55dad698afe7551a26b6acce15f3557600a6fd
mobeets <mobeets@gmail.com>
2014-06-19 09:43:08 -0500

    simulated annealing example

--- bin/examples.py
+++ bin/examples.py
@@ -20,7 +20,7 @@ def example_1():
     plt.xlabel('x')
     plt.ylabel('normalized count')
     plt.legend()
-    # plt.savefig('img/example-1.png')
+    # plt.savefig('../img/example-1.png')
     plt.show()
 
 def example_2():
@@ -37,12 +37,35 @@ def example_2():
     print 'Generated {0} samples after pruning from {1}.'.format(len(ahs), len(ahs0))
     print min(ahs), max(ahs)
 
-    plt.hist(ahs, 100, normed=True, label='theta-hat')
     plt.axvline(a, label='theta', color='b', linestyle='--')
+    plt.hist(ahs, 100, normed=True, label='pruned m-h samples')
     plt.legend()
-    # plt.savefig('img/example-2.png')
+    plt.savefig('../img/example-2.png')
+    plt.show()
+
+def example_3():
+    a, c = 3, 1
+    data = exponweib.rvs(a, c, size=1000)
+    l = round(max(data) - min(data))
+    pf = lambda ah, ch=c, d=data: np.sum(np.log(exponweib.pdf(data, ah, ch)))
+
+    d = 1.0
+    Tf = lambda i: d/np.log(i+2) # cooling function
+
+    e = 1 # step size of random-walk
+    qrf = lambda x, e=e: norm.rvs(x, e)
+    ahs0 = metropolis_hastings(2, 100000, pf, qrf, Tf)
+    print 'Generated {0} samples. MAP estimate is {1}'.format(len(ahs0), ahs0[-1])
+
+    plt.hist(ahs0, 100, normed=True, label='samples')
+    plt.axvline(a, label='theta', color='b', linestyle='--')
+    plt.axvline(ahs0[-1], label='theta-hat', color='c', linestyle='--')
+    plt.xlim([2.8, 3.4])
+    plt.legend()
+    plt.savefig('../img/example-3.png')
     plt.show()
 
 if __name__ == '__main__':
-    example_1()
-    # example_2()
+    # example_1()
+    example_2()
+    # example_3()
